5fd5e272cb3b41a8bc6b3ebf5c56b0581f8e91a9
Krishnaduth <krishnaduth777@gmail.com>
2024-08-12 06:51:14 +0530

    Completed till reservation form validation

---
+++ src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css']
+})
+export class ReservationFormComponent implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    // console.log("initialising form🕺🕺🕺🕺🕺");
+    
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ["", Validators.required],
+      checkOutDate: ["", Validators.required],
+      guestName: ["", Validators.required],
+      guestEmail: ["", [Validators.required,Validators.email]],
+      roomNumber: ["", Validators.required]
+    })
+  }
+
+
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log("Valid.😊");
+    }
+    else {
+      console.log("Not valid.😢");
+
+    }
+  }
+}
